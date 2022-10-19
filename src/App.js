d3eba364efde1c0773f6b74eb6ce88d65eeb8c29
catfoodtaster <59464329+catfoodtaster@users.noreply.github.com>
2022-10-19 14:20:09 +0700

    fix: remove double footer section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from './components/Header';
-import Main from './components/Footer';
+import Main from './components/Main';
 import Footer from './components/Footer';
 import Utils from './components/Utils';
 
