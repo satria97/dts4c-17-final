3e8a382c648a17b492467272049e72298fdb17cc
satria97 <satria.code97@gmail.com>
2022-10-19 15:14:00 +0700

    set footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,18 @@
 import './index.css';
+import { useState, useEffect } from 'react';
 
 export default function Footer() {
+  const [date, setDate] = useState();
+
+  const getYear = () => setDate(new Date().getFullYear())
+
+  useEffect(() => {
+    getYear();
+  }, []);
+
   return (
     <footer className="footer">
-      <p>Clipping &#169;2022</p>
+      <p>Clipping &#169;{date}</p>
     </footer>
   )
 }
