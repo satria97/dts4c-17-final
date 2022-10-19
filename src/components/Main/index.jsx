f938e4b77cb48bbacc76bf89fce39bd00a4b53d4
catfoodtaster <59464329+catfoodtaster@users.noreply.github.com>
2022-10-19 15:00:36 +0700

    feat: Add responsive header component

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,5 +1,13 @@
+import NewsBanner from './NewsBanner';
+import NewsFeed from './NewsFeed';
+
 import './index.css';
 
 export default function Main() {
-  return <></>;
+  return (
+    <main>
+      <NewsBanner />
+      <NewsFeed />
+    </main>
+  );
 }
