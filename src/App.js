4b51f6eeba8f581bdc52115ba293b59172f22fd8
catfoodtaster <59464329+catfoodtaster@users.noreply.github.com>
2022-10-19 09:47:07 +0700

    style: Add base styling to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import Header from './components/Header';
+import Main from './components/Footer';
+import Footer from './components/Footer';
+import Utils from './components/Utils';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app-container">
+      <Header />
+      <Main />
+      <Footer />
+      <Utils />
     </div>
   );
 }
