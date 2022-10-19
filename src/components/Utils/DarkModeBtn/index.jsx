bbb8ee2cfcfdf97656cf1fa63a00366f9cefdb6e
catfoodtaster <59464329+catfoodtaster@users.noreply.github.com>
2022-10-19 10:21:54 +0700

    feat: Implement dark mode and toast notif

---
+++ src/components/Utils/DarkModeBtn/index.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-regular-svg-icons';
+
+import { displayToast } from '../ToastNotif';
+
+import './index.css';
+
+export default function DarkModeBtn() {
+  const DARK_MODE_STATUS = 'DARK_MODE_STATUS';
+  const [darkMode, setDarkMode] = useState(false);
+
+  const html = document.documentElement;
+
+  useEffect(() => {
+    const darkModeStatus = localStorage.getItem(DARK_MODE_STATUS);
+
+    if (darkModeStatus === 'true') {
+      html.classList.add('dark');
+      setDarkMode(true);
+    } else {
+      html.classList.remove('dark');
+      setDarkMode(false);
+    }
+  });
+
+  function toggleDarkMode() {
+    if (html.classList.contains('dark')) {
+      html.classList.remove('dark');
+      setDarkMode(false);
+
+      displayToast('Dark mode off 🌞');
+
+      localStorage.setItem(DARK_MODE_STATUS, false);
+    } else {
+      html.classList.add('dark');
+      setDarkMode(true);
+
+      displayToast('Dark mode on 🌙');
+
+      localStorage.setItem(DARK_MODE_STATUS, true);
+    }
+  }
+
+  return (
+    <button
+      type="button"
+      className="dark-mode-btn"
+      id="dark-mode"
+      title="Toggle dark mode"
+      onClick={toggleDarkMode}
+    >
+      <FontAwesomeIcon icon={darkMode ? faMoon : faSun} />
+    </button>
+  );
+}
