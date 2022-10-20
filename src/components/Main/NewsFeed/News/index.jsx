2ebee45437b2fcd9842185f497de0227c948ddee
satria97 <satria.code97@gmail.com>
2022-10-20 13:35:03 +0700

    Set layout news

--- src/components/Main/NewsFeed/News/index.jsx
+++ src/components/Main/NewsFeed/News/index.jsx
@@ -3,14 +3,16 @@ import './index.css';
 export default function News() {
   return (
     <div className="news">
-      <img
-        src="https://source.unsplash.com/random"
-        alt="sample newsitem"
-        className="newsitem-photo"
-      />
+      <div className='news-image'>
+        <img
+          src="https://source.unsplash.com/random"
+          alt="sample newsitem"
+          className="newsitem-photo"
+        />
+      </div>
       <div className="news-info">
         <div className="news-info-title">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam, non?
+          News Title Lorem Ipsum Dolor Sit Amet
         </div>
         <div className="news-info-detail">
           <div className="news-info-publication">2 hours ago</div>
