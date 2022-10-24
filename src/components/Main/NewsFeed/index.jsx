7006a7e5920b328be6035b7ac67a7cea10e1828c
catfoodtaster <59464329+catfoodtaster@users.noreply.github.com>
2022-10-24 13:59:22 +0700

    feat: Fetch articles from newsapi

--- src/components/Main/NewsFeed/index.jsx
+++ src/components/Main/NewsFeed/index.jsx
@@ -1,27 +1,38 @@
-import NewsBanner from './NewsBanner';
-import News from './News';
+import { useState, useEffect } from 'react';
+
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
+
+import NewsItem from './NewsItem';
 
 import './index.css';
 
-const NewsFeed = () => {
+export default function NewsFeed() {
+  const [news, setNews] = useState([]);
+
+  const API_KEY = process.env.REACT_APP_NEWSAPI_API_KEY;
+  const API_URL = 'https://newsapi.org/v2/everything?q=indonesia&apiKey=';
+
+  useEffect(() => {
+    (async () => {
+      const res = await axios.get(API_URL + API_KEY);
+      
+      setNews(res.data.articles);
+    })();
+  }, []);
+
   return (
-    <div className="newsfeed">
-      <NewsBanner />
-      <div className="latest-news">
-        <h1 className="latest-news-heading">Latest News</h1>
-        <div className="news-container">
-          <News />
-          <News />
-          <News />
-          <News />
-          <News />
-          <News />
-          <News />
-          <News />
-        </div>
-      </div>
+    <div className="news-container">
+      {news.map((item) => (
+        <NewsItem
+          key={uuidv4()}
+          title={item.title}
+          cover={item.urlToImage}
+          source={item.source.name}
+          time="two hours ago"
+          description={item.description}
+        />
+      ))}
     </div>
   );
 }
-
-export default NewsFeed;
