e435a1844be4b977726477753ca53a654ccc732c
catfoodtaster <59464329+catfoodtaster@users.noreply.github.com>
2022-10-24 09:32:15 +0700

    feat: Fetch news using newsapi
    - Temporary design overhaul

---
+++ src/components/Main/NewsFeed/NewsItem/index.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+
+import './index.css';
+
+export default function NewsItem({
+  title,
+  cover,
+  time,
+  source,
+  description,
+  content,
+}) {
+  return (
+    <div className="news">
+      <img src={cover} alt={title} />
+      <div className="news-info">
+        <div className="news-info-title">{title}</div>
+        <div className="news-info-detail">
+          <div className="news-info-publication">{time}</div>
+          <div className="news-info-source">{source}</div>
+        </div>
+      </div>
+      <p className="news-preview">&quot;{description}&quot;</p>
+      <p className="news-content">{content}</p>
+    </div>
+  );
+}
--- src/components/Main/NewsFeed/index.jsx
+++ src/components/Main/NewsFeed/index.jsx
@@ -1,23 +1,37 @@
-import NewsBanner from './NewsBanner';
-import News from './News';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import NewsItem from './NewsItem';
 
 import './index.css';
 
 export default function NewsFeed() {
+  const [news, setNews] = useState([]);
+
+  const API_KEY = process.env.REACT_APP_NEWSAPI_API_KEY;
+  const API_URL = 'https://newsapi.org/v2/everything?q=indonesia&apiKey=';
+  // const SEARCH_TAG =
+
+  useEffect(() => {
+    (async () => {
+      const res = await axios.get(API_URL + API_KEY);
+      setNews(res.data.articles);
+    })();
+  }, []);
+
   return (
     <div className="newsfeed">
-      <NewsBanner />
       <div className="latest-news">
-        <h1 className="latest-news-heading">Latest News</h1>
         <div className="news-container">
-          <News />
-          <News />
-          <News />
-          <News />
-          <News />
-          <News />
-          <News />
-          <News />
+          {news.map((item) => (
+            <NewsItem
+              title={item.title}
+              cover={item.urlToImage}
+              source={item.source.name}
+              time="two hours ago"
+              description={item.description}
+            />
+          ))}
         </div>
       </div>
     </div>
