4c1d4e0f2dbf19a967970a8d64495a02dfc7ce7c
catfoodtaster <59464329+catfoodtaster@users.noreply.github.com>
2022-10-25 20:36:25 +0700

    fix: Keep footer at the bottom of the page

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,19 @@
-import Copyright from './Copyright';
+import { useState, useEffect } from 'react';
 
 import './index.css';
 
 export default function Footer() {
+  const [date, setDate] = useState();
+
+  const getYear = () => setDate(new Date().getFullYear());
+
+  useEffect(() => {
+    getYear();
+  }, []);
+
   return (
     <footer className="footer">
-      <Copyright />
+      <small>&#169;{date}</small>
     </footer>
   );
 }
