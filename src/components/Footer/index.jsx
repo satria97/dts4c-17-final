98deb6af31a1a8146a8eb228399d7be224de0a03
catfoodtaster <59464329+catfoodtaster@users.noreply.github.com>
2022-10-25 16:30:10 +0700

    refac: Segregate 'copyright' from footer
    Komponen copyright bisa digunakan berulang-ulang

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,18 +1,11 @@
+import Copyright from './Copyright';
+
 import './index.css';
-import { useState, useEffect } from 'react';
 
 export default function Footer() {
-  const [date, setDate] = useState();
-
-  const getYear = () => setDate(new Date().getFullYear())
-
-  useEffect(() => {
-    getYear();
-  }, []);
-
   return (
     <footer className="footer">
-      <small>Clipping &#169;{date}</small>
+      <Copyright />
     </footer>
-  )
+  );
 }
