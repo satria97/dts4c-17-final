409895bfe5d449128b5a39aa1b62c216f52fe5c0
catfoodtaster <59464329+catfoodtaster@users.noreply.github.com>
2022-10-26 19:54:01 +0700

    fix: Switch API provider
    API yang lama tidak berjalan saat deployment

--- src/components/Main/NewsFeed/index.jsx
+++ src/components/Main/NewsFeed/index.jsx
@@ -12,28 +12,37 @@ import './index.css';
 export default function NewsFeed() {
   const [news, setNews] = useState([]);
 
-  const API_KEY = process.env.REACT_APP_NEWSAPI_API_KEY;
-  const API_URL = 'https://newsapi.org/v2/everything?q=indonesia&apiKey=';
+  // const API_KEY = process.env.REACT_APP_NEWSAPI_API_KEY;
+  const API_URL = 'https://berita-indo-api.vercel.app/v1/cnn-news/';
 
   useEffect(() => {
     (async () => {
-      const res = await axios.get(API_URL + API_KEY);
+      const res = await axios.get(API_URL);
 
-      setNews(res.data.articles);
+      setNews(res.data.data);
     })();
-  }, []);
+  }, [API_URL]);
 
   return (
     <div className="news-container">
       {news.map((item) => (
+        // <NewsItem
+        //   key={uuidv4()}
+        //   title={item.title.replace(/_[^_]+$/g, '')}
+        //   cover={item.urlToImage ? item.urlToImage : NoImage}
+        //   source={item.source.name}
+        //   time={item.publishedAt.slice(0, 10)}
+        //   description={item.description}
+        //   content={item.url}
+        // />
         <NewsItem
           key={uuidv4()}
-          title={item.title.replace(/_[^_]+$/g, '')}
-          cover={item.urlToImage ? item.urlToImage : NoImage}
-          source={item.source.name}
-          time={item.publishedAt.slice(0, 10)}
-          description={item.description}
-          content={item.url}
+          title={item.title}
+          cover={item.image.large ? item.image.large : NoImage}
+          source="CNN"
+          time={item.isoDate.slice(0, 10)}
+          description={item.contentSnippet}
+          content={item.link}
         />
       ))}
     </div>
