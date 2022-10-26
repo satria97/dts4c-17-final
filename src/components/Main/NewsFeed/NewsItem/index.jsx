b1be0a98bceff4a0e3976317898b666318928d13
catfoodtaster <59464329+catfoodtaster@users.noreply.github.com>
2022-10-26 16:40:42 +0700

    feat: Add pop-up detail news

--- src/components/Main/NewsFeed/NewsItem/index.jsx
+++ src/components/Main/NewsFeed/NewsItem/index.jsx
@@ -1,3 +1,5 @@
+import LaunchOutlinedIcon from '@mui/icons-material/LaunchOutlined';
+
 import './index.css';
 
 export default function NewsItem({
@@ -8,12 +10,16 @@ export default function NewsItem({
   description,
   content,
 }) {
-  function goToDetail(url) {
-    window.open(url, '_blank');
+  function showDetail(e) {
+    const allNews = document.querySelectorAll('.news');
+
+    if (e.currentTarget !== allNews[0]) {
+      e.currentTarget.classList.toggle('detail');
+    }
   }
 
   return (
-    <div className="news" onClick={() => goToDetail(content)}>
+    <div className="news" onClick={showDetail}>
       <img src={cover} alt={title} />
       <div className="news-info">
         <div className="news-info-title">{title}</div>
@@ -23,6 +29,11 @@ export default function NewsItem({
         </div>
       </div>
       <p className="news-preview">&quot;{description}&quot;</p>
+      <button className="read-more" title="read more">
+        <a href={content} target="_blank" rel="noreferrer">
+          <LaunchOutlinedIcon />
+        </a>
+      </button>
     </div>
   );
 }
